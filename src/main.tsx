783f56a76c80c77e5880a8d2ddbba1cd3e5aadf3
tektechnologies <craigwbarkley@icloud.com>
2025-10-23 22:18:43 -0500

    add phosphorus icons

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import "./theme/index.css";
+import React from "react";
 
 const router = createRouter({ routeTree });
 
